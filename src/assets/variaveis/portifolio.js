f3e48b078fa31baa3c9d197a724a79c63747f3cc
Uiaran Guilherme <uiaran@hotmail.com>
2022-01-14 02:24:56 -0300

    meu ultimo commit

--- src/assets/variaveis/portifolio.js
+++ src/assets/variaveis/portifolio.js
@@ -13,49 +13,6 @@ const Portifolio = [
         ],
         video: CalculadoraVideo
     },
-    {
-        id: 1,
-        title: "Calculadora",
-        description: "Emais a fundo funções em JS (JavaScript), com ele desenvolvi melhor a lógica de programação, função orientada a objetos. Também desenvolvi melhor os conceitos do front-end como flex-box e de disigner. Como um dos primeiros trabalhos tive o resultado com bastante entusiasmo, pois compreendi ali meu desejo de aprender a programar.",
-        tecnologia: [
-            <FaHtml5/>,
-            <FaCss3Alt/>,
-            <FaJsSquare/>
-        ],
-        video: "link"
-    },
-    {
-        id: 2,
-        title: "Calculadora",
-        description: "Este projeto com o intuito de aprender, com ele desenvolvi melhor a lógica de programação, função orientada a objetos. Também desenvolvi melhor os conceitos do front-end como flex-box e de disigner. Como um dos primeiros trabalhos tive o resultado com bastante entusiasmo, pois compreendi ali meu desejo de aprender a programar.",
-        tecnologia: [
-            <FaHtml5/>,
-            <FaCss3Alt/>,
-            <FaJsSquare/>
-        ],
-        video: "link"
-    },
-    {
-        id: 3,
-        title: "Calculadora",
-        description: "Este  o intuito de aprender mais a fundo funções em JS (JavaScript), com ele desenvolvi melhor a lógica de programação, função orientada a objetos. Também desenvolvi melhor os conceitos do front-end como flex-box e de disigner. Como um dos primeiros trabalhos tive o resultado com bastante entusiasmo, pois compreendi ali meu desejo de aprender a programar.",
-        tecnologia: [
-            <FaHtml5/>,
-            <FaCss3Alt/>,
-            <FaJsSquare/>
-        ],
-        video: "link"
-    },
-    {
-        id: 4,
-        title: "Calculadora",
-        description: " com o intuito de aprender mais a fundo funções em JS (JavaScript), com ele desenvolvi melhor a lógica de programação, função orientada a objetos. Também desenvolvi melhor os conceitos do front-end como flex-box e de disigner. Como um dos primeiros trabalhos tive o resultado com bastante entusiasmo, pois compreendi ali meu desejo de aprender a programar.",
-        tecnologia: [
-            <FaHtml5/>,
-            <FaCss3Alt/>,
-            <FaJsSquare/>
-        ],
-        video: 'link'
-    },
+    
 ]
 export default Portifolio
